51adccbdc1a22bfae54e12f7ef7d56a0a7061edb
rareold14 <j.pedro15dragon@gmail.com>
2023-07-26 17:16:43 -0300

    pá

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
           </a>
           <div className="windows-10">
             <img src={Windows10} alt="" className="windows-logo"/>
-            <p>A partir de R$ 2.641,00</p>
+            <p className='windows-preco'>A partir de R$ 2.641,00</p>
             <a href='https://strongtechbrasil.com.br/produto/computador-completo-all-in-one-intel-i7-8gb-ssd-512gb-215-full-hd-webcam-teclado-e-mouse-sem-fio-strong-tech/?add-to-cart=10015'>
               <button className="link-strong">Comprar</button>
             </a></div>
